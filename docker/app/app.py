3f985f6ca9bd29bef17def226251e0db88c17b1e
wildcad <wildi@mail.ru>
2025-05-11 19:36:08 +0300

    Финальные исправления для Docker контейнера

--- docker/app/app.py
+++ docker/app/app.py
@@ -1,4 +1,7 @@
 from explainerdashboard import ExplainerDashboard
 
+# Загружаем дашборд из файла
 db = ExplainerDashboard.from_config("abalone_regression_dashboard.yaml")
-db.run(host='0.0.0.0', port=8050, use_waitress=True)
+
+if __name__ == "__main__":
+    db.run(host='0.0.0.0', port=8050)
